15730ce24bd0413204b56ac5af66ac172e77b3bd
JohnF <fricanojohnb@gmail.com>
2019-09-02 15:51:52 -0500

    changed wallmovement to lock axis, object types in unity dont seem to support this when working directly with the transform object of gameobjects

--- thatboxgame/Assets/Scripts/Walls/WallMovement.cs
+++ thatboxgame/Assets/Scripts/Walls/WallMovement.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class WallMovement : MonoBehaviour
 {
     private Vector3 offset;
+    public int slideDistance = 0;
+    public bool xWall = false;
+    public bool yWall = false;
 
     void OnMouseDown()
     {
         offset = gameObject.transform.position -
-        Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        Camera.main.ScreenToWorldPoint(new Vector3(Mathf.Round(Input.mousePosition.x), Mathf.Round(Input.mousePosition.y)));
     }
 
     void OnMouseDrag()
     {
-        Vector3 newPosition = new Vector3(Mathf.Round(Input.mousePosition.x), Mathf.Round(Input.mousePosition.y, 10.0f));
-        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        if (xWall)
+        {
+            Vector3 newPosition = new Vector3(Mathf.Round(Input.mousePosition.x), gameObject.transform.position.y);
+            transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        }
+        else if (yWall)
+        {
+            Vector3 newPosition = new Vector3(gameObject.transform.position.x, Mathf.Round(Input.mousePosition.y));
+            transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        }
     }
 }

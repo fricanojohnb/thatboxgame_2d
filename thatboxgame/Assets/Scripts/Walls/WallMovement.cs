dfc004171b33d2f95b4259e813e8a6f9469d6a72
Charles Neff <charlesneff13@gmail.com>
2019-09-02 15:13:00 -0500

    Movement

---
+++ thatboxgame/Assets/Scripts/Walls/WallMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour
+{
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        offset = gameObject.transform.position -
+        Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 newPosition = new Vector3(Mathf.Round(Input.mousePosition.x), Mathf.Round(Input.mousePosition.y, 10.0f));
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+    }
+}

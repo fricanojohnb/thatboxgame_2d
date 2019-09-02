3e86b23bf784ef7b34d28ebf67121256254bbbf2
Charles Neff <charlesneff13@gmail.com>
2019-09-02 16:53:37 -0500

    added broken movement...

---
+++ thatboxgame/Assets/Scripts/Walls/XWallMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XWallMovement : MonoBehaviour
+{
+    private Vector3 offset;
+    public int slideDistance = 0;
+    private bool isColliding = false;
+    private bool mouseClicked = false;
+
+    void OnMouseDown()
+    {
+        offset = gameObject.transform.position -
+        Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0f));
+        mouseClicked = true;
+    }
+
+    private void OnMouseUp() {
+        mouseClicked = false;
+    }
+
+    private void Update()
+    {
+        if (!isColliding && mouseClicked)
+        {
+            Vector3 newPosition = new Vector3(Input.mousePosition.x, 0);
+            transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isColliding = true;
+        transform.position = transform.position - offset;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isColliding = false;
+    }
+}
---
+++ thatboxgame/Assets/Scripts/Walls/YWallMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YWallMovement : MonoBehaviour
+{
+    private Vector3 offset;
+    public int slideDistance = 0;
+    private bool mouseClicked = false;
+    private bool isColliding = false;
+
+    void OnMouseDown()
+    {
+        offset = gameObject.transform.position -
+        Camera.main.ScreenToWorldPoint(new Vector3(0f, Input.mousePosition.y));    
+        mouseClicked = true;
+    }
+
+    private void OnMouseUp() {
+        mouseClicked = false;
+    }
+
+    private void Update()
+    {
+        if (!isColliding && mouseClicked){
+            Vector3 newPosition = new Vector3(0, Input.mousePosition.y);
+            transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other) {      
+        isColliding = true;
+        transform.position = transform.position - offset;
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        isColliding = false;
+    }
+}
